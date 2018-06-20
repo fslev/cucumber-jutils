1180dde597f02163af5229fcf39afce73ce74113
Florin Slevoaca <florin.slevoaca@1and1.ro>
2018-06-20 19:49:52 +0300

    slow progress

--- src/test/java/tests/SimpleTest.java
+++ src/test/java/tests/SimpleTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", glue = {"stepdefs"},
+@CucumberOptions(features = "src/test/resources/features/simple.feature", glue = {"stepdefs"},
         dryRun = false,plugin = {"pretty","html:target/cucumber-html-report"})
 public class SimpleTest {
 }
