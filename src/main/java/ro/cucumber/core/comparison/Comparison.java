5887c49bd3f659645906c7bd491a31077cd2366a
Florin <slevoaca.florin@gmail.com>
2018-07-15 22:36:13 +0300

    Add comparison. Does not build

--- src/main/java/ro/cucumber/core/comparison/Comparison.java
+++ src/main/java/ro/cucumber/core/comparison/Comparison.java
@@ -1,4 +1,21 @@
 package ro.cucumber.core.comparison;
 
+import java.util.Map;
+
+import static org.junit.Assert.fail;
+
 public class Comparison {
+    private String expected;
+    private String actual;
+
+    public Comparison(String expected, String actual) {
+        this.expected = expected;
+        this.actual = actual;
+    }
+
+    Map<String, String> compare() {
+        if ((actual == null) != (expected == null)) {
+            fail(String.format("Expected: [%s] But found: [%s]", expected, actual));
+        }
+    }
 }
