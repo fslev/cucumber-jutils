8b7915781b699e4cd3774bdbf3744f4972eafabe
Florin Slevoaca <florin.slevoaca@1and1.ro>
2018-07-19 18:37:07 +0300

    Add universal matcher

--- src/main/java/ro/cucumber/core/matchers/Matcher.java
+++ src/main/java/ro/cucumber/core/matchers/Matcher.java
@@ -1,13 +1,14 @@
 package ro.cucumber.core.matchers;
 
 import ro.cucumber.core.matchers.exceptions.MatcherException;
-import ro.cucumber.core.symbols.SymbolAssignable;
+import java.util.HashMap;
 import java.util.Map;
 import static org.junit.Assert.fail;
 
-public class Matcher implements SymbolAssignable, Matchable {
+public class Matcher implements SymbolsAssignMatchable {
     protected Object expected;
     protected Object actual;
+    private Map<String, String> assignSymbols = new HashMap<>();
 
     public Matcher(Object expected, Object actual) throws MatcherException {
         this.expected = expected;
@@ -15,14 +16,22 @@ public class Matcher implements SymbolAssignable, Matchable {
     }
 
     @Override
-    public void matches() {
+    public void match() {
         if ((actual == null) != (expected == null)) {
             fail(String.format("Expected: [%s] But found: [%s]", expected, actual));
         }
+        SymbolsAssignMatchable matcher;
+        try {
+            matcher = new JsonMatcher(expected, actual);
+        } catch (MatcherException e) {
+            matcher = new StringRegexMatcher(expected, actual);
+        }
+        matcher.match();
+        assignSymbols = matcher.getAssignSymbols();
     }
 
     @Override
     public Map<String, String> getAssignSymbols() {
-        return null;
+        return assignSymbols;
     }
 }
