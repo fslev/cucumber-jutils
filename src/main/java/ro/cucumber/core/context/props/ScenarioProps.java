3931ab9441142ba73cf8805a8c8e952e1d55a264
Florin Slevoaca <florin.slevoaca@1and1.ro>
2018-07-27 19:36:14 +0300

    UUID support

--- src/main/java/ro/cucumber/core/context/props/ScenarioProps.java
+++ src/main/java/ro/cucumber/core/context/props/ScenarioProps.java
@@ -3,13 +3,19 @@ package ro.cucumber.core.context.props;
 import cucumber.runtime.java.guice.ScenarioScoped;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @ScenarioScoped
 public class ScenarioProps {
     private Map<String, Object> props = new HashMap<>();
 
     public Object get(String key) {
-        return props.get(key);
+        switch (key) {
+            case "UUID":
+                return getUUID();
+            default:
+                return props.get(key);
+        }
     }
 
     public void put(String key, Object val) {
@@ -18,5 +24,9 @@ public class ScenarioProps {
 
     public void putAll(Map<String, Object> props) {
         this.props.putAll(props);
+    }
+
+    private String getUUID() {
+        return UUID.randomUUID().toString();
     }
 }
