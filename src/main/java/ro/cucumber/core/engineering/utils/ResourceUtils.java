6a1a435d33ff2e778dcc99fc86c983f53be6cedd
Florin Slevoaca <florin.slevoaca@1and1.ro>
2018-11-02 16:59:10 +0200

    Add resource utility that walks a directory and reads each file Refactoring. Use at least java 1.9

--- src/main/java/ro/cucumber/core/engineering/utils/ResourceUtils.java
+++ src/main/java/ro/cucumber/core/engineering/utils/ResourceUtils.java
@@ -1,15 +1,42 @@
 package ro.cucumber.core.engineering.utils;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
-import com.fasterxml.jackson.databind.JsonNode;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ResourceUtils {
 
     public static String read(String filePath) {
-        try (InputStream is = ResourceUtils.class.getClassLoader().getResourceAsStream(filePath);
+        return readFromAbsolutePath(Thread.currentThread().getContextClassLoader().getResource(filePath).getPath());
+    }
+
+    /**
+     * @return a list of Map<String,String> data for each file within the directory and its sub-directories
+     * A key-value pair from the map contains the file path respectively the file content
+     */
+    public static List<Map<String, String>> readDirectory(String dirPath) {
+        try {
+            Path base = Paths.get(Thread.currentThread().getContextClassLoader().getResource(dirPath).toURI());
+            List<Map<String, String>> dataList = Files.walk(base).filter(path -> path.toFile().isFile())
+                    .map(path -> Map.of(base.relativize(path).toString(), readFromAbsolutePath(path.toString())))
+                    .collect(Collectors.toList());
+            return dataList;
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String readFromAbsolutePath(String filePath) {
+        try (InputStream is = new FileInputStream(filePath);
              ByteArrayOutputStream result = new ByteArrayOutputStream()) {
             byte[] buffer = new byte[1024];
             int length;
@@ -19,14 +46,6 @@ public class ResourceUtils {
             return result.toString(StandardCharsets.UTF_8.name());
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    public static JsonNode readToJson(String filePath) {
-        try {
-            return JsonUtils.toJson(read(filePath));
-        } catch (IOException e) {
-            throw new RuntimeException("Malformed JSON", e);
         }
     }
 }
--- src/test/java/ro/cucumber/core/resource/ResourceReadTests.java
+++ src/test/java/ro/cucumber/core/resource/ResourceReadTests.java
@@ -1,7 +1,12 @@
 package ro.cucumber.core.resource;
 
+import ro.cucumber.core.engineering.utils.JsonUtils;
 import ro.cucumber.core.engineering.utils.ResourceUtils;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -14,9 +19,22 @@ public class ResourceReadTests {
     }
 
     @Test
-    public void testJsonReadingFromFile() {
+    public void testJsonReadingFromFile() throws IOException {
         ObjectNode json = JsonNodeFactory.instance.objectNode();
         json.putObject("menu").put("show", true);
-        assertEquals(json, ResourceUtils.readToJson("foobar/file1.json"));
+        assertEquals(json, JsonUtils.toJson(ResourceUtils.read("foobar/file1.json")));
+    }
+
+    @Test
+    public void testRecursiveReaderFromDirectory() {
+        String dirpath = "foobar/dir";
+        List<Map<String, String>> actualData = ResourceUtils.readDirectory(dirpath);
+        assertEquals(5, actualData.size());
+        assertTrue(actualData.contains(Map.of("foobar1.json", "1")));
+        assertTrue(actualData.contains(Map.of("foo/foo1.json", "2")));
+        assertTrue(actualData.contains(Map.of("foo/foo2.json", "3")));
+        assertTrue(actualData.contains(Map.of("foo/bar/bar1.json", "4")));
+        assertTrue(actualData.contains(Map.of("foo/bar/bar2.json", "5")));
+
     }
 }
