2f4f9956dca51a92daeed0e72e008191b7f1b202
Florin Slevoaca <florin.slevoaca@ionos.com>
2019-04-04 11:00:06 +0300

    bug fix

--- Changelog.md
+++ Changelog.md
@@ -1,3 +1,6 @@
+### 3.17  
+Fix Http Response Listener bug       
+
 ### 3.16  
 Add SHELL support     
 
--- src/main/java/com/cucumber/utils/clients/http/HttpResponseLoggerInterceptor.java
+++ src/main/java/com/cucumber/utils/clients/http/HttpResponseLoggerInterceptor.java
@@ -24,8 +24,11 @@ public class HttpResponseLoggerInterceptor implements HttpResponseInterceptor {
         log.debug("Response STATUS: {}", response.getStatusLine());
         log.debug("Response HEADERS: {}", Arrays.asList(response.getAllHeaders()));
         log.debug("Response BODY:{}{}", () -> System.lineSeparator(), () -> {
-            HttpEntity entity = response.getEntity();
             if (response == null) {
+                return null;
+            }
+            HttpEntity entity = response.getEntity();
+            if (entity == null) {
                 return null;
             }
             String content = null;
