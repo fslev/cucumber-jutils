530479104cbacdc502934a78df926c039b1701e9
Florin Slevoaca <florin.slevoaca@ionos.com>
2019-04-15 18:09:57 +0300

    remove whitespace

--- src/test/java/com/cucumber/utils/features/CucumberUtilsTest.java
+++ src/test/java/com/cucumber/utils/features/CucumberUtilsTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features",
         glue = {"com.cucumber.utils"}, plugin = {"pretty", "junit:output",
-        "json:target/cucumber-report/report.json"}, tags = {"not @Ignore", " not @ignore"})
+        "json:target/cucumber-report/report.json"}, tags = {"not @Ignore", "not @ignore"})
 public class CucumberUtilsTest {
 }
 
