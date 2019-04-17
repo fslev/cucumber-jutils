1c06e56a1834718f563a276957149c569472ee2f
Florin Slevoaca <florin.slevoaca@ionos.com>
2019-04-17 11:15:18 +0300

    refactor testng reporting

--- src/test/java/com/cucumber/utils/features/CucumberUtilsTest.java
+++ src/test/java/com/cucumber/utils/features/CucumberUtilsTest.java
@@ -4,17 +4,40 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 import org.junit.runner.RunWith;
+import org.testng.ITest;
+import org.testng.ITestContext;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
+
+import java.lang.reflect.Method;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features",
         glue = {"com.cucumber.utils"}, plugin = {"pretty", "junit:output",
         "json:target/cucumber-report/report.json"}, tags = {"not @Ignore", "not @ignore"})
-public class CucumberUtilsTest extends AbstractTestNGCucumberTests {
+public class CucumberUtilsTest extends AbstractTestNGCucumberTests implements ITest {
+
+    private ThreadLocal<String> testName = new ThreadLocal<>();
+
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
         return super.scenarios();
+    }
+
+
+    @BeforeMethod
+    public void BeforeMethod(Method method, Object[] testData, ITestContext ctx) {
+        if (testData.length > 0) {
+            testName.set(testData[0].toString());
+            ctx.setAttribute("testName", testName.get());
+        } else
+            ctx.setAttribute("testName", method.getName());
+    }
+
+    @Override
+    public String getTestName() {
+        return testName.get();
     }
 }
 
