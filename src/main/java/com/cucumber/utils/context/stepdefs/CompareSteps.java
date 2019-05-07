46a89a8d2a229ed9aeff240a1648552b65bfd34f
Florin Slevoaca <florin.slevoaca@ionos.com>
2019-05-07 12:35:56 +0300

    Add logger to comparison

--- src/main/java/com/cucumber/utils/context/stepdefs/CompareSteps.java
+++ src/main/java/com/cucumber/utils/context/stepdefs/CompareSteps.java
@@ -4,32 +4,41 @@ import com.cucumber.utils.context.utils.Cucumbers;
 import com.google.inject.Inject;
 import cucumber.api.java.en.Then;
 import cucumber.runtime.java.guice.ScenarioScoped;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 @ScenarioScoped
 public class CompareSteps {
 
+    private Logger log = LogManager.getLogger();
+
     @Inject
     private Cucumbers cucumbers;
 
     @Then("COMPARE {} with \"{}\"")
     public void compareWithString(Object expected, Object actual) {
+        log.info("Compare {} against {}", expected, actual);
         cucumbers.compare(expected, actual);
     }
 
     @Then("COMPARE {} with content from path \"{}\"")
     public void compareWithContentFromFilepath(Object expected, String filePath) {
-        cucumbers.compare(expected, cucumbers.read(filePath));
+        String actual = cucumbers.read(filePath);
+        log.info("Compare {} against {}", expected, actual);
+        cucumbers.compare(expected, actual);
     }
 
     @Then("COMPARE {} with")
     public void compareWithDocString(Object expected, String actual) {
+        log.info("Compare {} against {}", expected, actual);
         cucumbers.compare(expected, actual);
     }
 
     @Then("COMPARE {} with table")
     public void compareWithDataTable(Object expected, List actual) {
+        log.info("Compare {} against {}", expected, actual);
         cucumbers.compare(expected, actual);
     }
 }
