6216910e498272ef489d8b964efac68d08a2f325
Florin Slevoaca <florin.slevoaca@ionos.com>
2019-09-13 19:01:30 +0300

    bug fix

--- src/main/java/com/cucumber/utils/engineering/compare/Compare.java
+++ src/main/java/com/cucumber/utils/engineering/compare/Compare.java
@@ -58,7 +58,7 @@ public class Compare implements Placeholdable {
     private boolean nullsMatch() {
         if (expected == null ^ actual == null) {
             fail(ParameterizedMessage.format("{}\nEXPECTED:\n[{}]\nBUT GOT:\n[{}]",
-                    new String[]{message != null ? message : "", expected.toString(), actual.toString()}));
+                    new String[]{message != null ? message : "", (String) expected, (String) actual}));
         } else if (expected == null) {
             return true;
         }
