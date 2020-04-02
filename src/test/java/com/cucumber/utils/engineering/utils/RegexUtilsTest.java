e8176cddd6d7c585c81a1efef1cf4c90e237bb39
Florin Slevoaca <florin.slevoaca@ionos.com>
2020-04-02 17:02:34 +0300

    add one more test

--- src/test/java/com/cucumber/utils/engineering/utils/RegexUtilsTest.java
+++ src/test/java/com/cucumber/utils/engineering/utils/RegexUtilsTest.java
@@ -2,7 +2,6 @@ package com.cucumber.utils.engineering.utils;
 
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
@@ -13,5 +12,11 @@ public class RegexUtilsTest {
     public void testStringContainsSpecialRegexCharacters() {
         String s = "[0-9]";
         assertEquals(Collections.singletonList("["), RegexUtils.getRegexCharsFromString(s));
+    }
+
+    @Test
+    public void testNullStringContainsSpecialRegexCharacters() {
+        String s = null;
+        assertEquals(Collections.emptyList(), RegexUtils.getRegexCharsFromString(s));
     }
 }
