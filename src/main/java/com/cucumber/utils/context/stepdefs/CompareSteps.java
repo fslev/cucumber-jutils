eb1bf7fcfd64987ef74d213d180314998b491960
Carmen <carmen.iorga@1and1.ro>
2020-04-03 16:26:25 +0300

    refactor

--- src/main/java/com/cucumber/utils/context/stepdefs/CompareSteps.java
+++ src/main/java/com/cucumber/utils/context/stepdefs/CompareSteps.java
@@ -36,12 +36,11 @@ public class CompareSteps {
         logger.log("    Negative Compare:\n{}\n    Against:\n{}", expected, actual);
         try {
             cucumbers.compare(expected, actual);
-            throw new AssertionError("Compared objects match");
         } catch (AssertionError e) {
-            if (e.getMessage().equals("Compared objects match"))
-                throw new AssertionError("Compared objects match");
             logger.log("Assertion Error caught. Negative compare passes {}", e.getMessage());
+            return;
         }
+        throw new AssertionError("Compared objects match");
     }
 
     @Then("COMPARE {} with content from path \"{}\"")
