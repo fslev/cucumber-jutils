7916f453e98c612a4cf085c00d437cf9d81d13dc
Florin Slevoaca <florin.slevoaca@ionos.com>
2020-04-08 18:12:48 +0300

    extract Xml class to separate file and keep its access to default

--- src/main/java/com/cucumber/utils/engineering/utils/XmlUtils.java
+++ src/main/java/com/cucumber/utils/engineering/utils/XmlUtils.java
@@ -1,6 +1,7 @@
 package com.cucumber.utils.engineering.utils;
 
-import org.w3c.dom.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -92,85 +93,5 @@ public class XmlUtils {
         Element element = toDocument(xml).getDocumentElement();
         Xml.walkAndProcessNode(element, processFunction, "", resultsMap);
         return resultsMap;
-    }
-}
-
-class Xml {
-
-    static <R> void walkAndProcessNode(Node node, Function<String, R> processFct, String parentPath, Map<String, R> results) {
-        R result = processNode(node, processFct);
-        String currentPath = getNodePath(node, parentPath);
-        if (result != null) {
-            results.put(currentPath, result);
-        }
-        if (node.hasAttributes()) {
-            NamedNodeMap attributes = node.getAttributes();
-            for (int i = 0; i < attributes.getLength(); i++) {
-                walkAndProcessNode(attributes.item(i), processFct, currentPath, results);
-            }
-        }
-        if (node.hasChildNodes()) {
-            NodeList nodeList = node.getChildNodes();
-            for (int i = 0; i < nodeList.getLength(); i++) {
-                Node item = nodeList.item(i);
-                walkAndProcessNode(item, processFct, currentPath, results);
-            }
-        }
-    }
-
-    private static <R> R processNode(Node node, Function<String, R> processFct) {
-        switch (node.getNodeType()) {
-            case Node.ELEMENT_NODE:
-                return processFct.apply(node.getNodeName());
-            case Node.ATTRIBUTE_NODE:
-            case Node.TEXT_NODE:
-                return processFct.apply(node.getNodeValue());
-        }
-        return null;
-    }
-
-    private static String getNodePath(Node node, String parentPath) {
-        switch (node.getNodeType()) {
-            case Node.ELEMENT_NODE:
-                if (isListElement(node)) {
-                    return parentPath + "/" + node.getNodeName() + "[" + findNodePositionAmongListElements(node) + "]";
-                } else {
-                    return parentPath + "/" + node.getNodeName();
-                }
-            case Node.ATTRIBUTE_NODE:
-                return parentPath + "{" + node.getNodeName() + "}";
-            default:
-                return parentPath;
-        }
-    }
-
-    private static boolean isListElement(Node node) {
-        Node prevNode = node.getPreviousSibling();
-        Node nextNode = node.getNextSibling();
-        while (prevNode != null) {
-            if (prevNode.getNodeType() == Node.ELEMENT_NODE && prevNode.getNodeName().equals(node.getNodeName())) {
-                return true;
-            }
-            prevNode = prevNode.getPreviousSibling();
-        }
-        while (nextNode != null) {
-            if (nextNode.getNodeType() == Node.ELEMENT_NODE && nextNode.getNodeName().equals(node.getNodeName())) {
-                return true;
-            }
-            nextNode = nextNode.getNextSibling();
-        }
-        return false;
-    }
-
-    private static int findNodePositionAmongListElements(Node node) {
-        int i = 1;
-        Node prevNode = node.getPreviousSibling();
-        while (prevNode != null) {
-            if (prevNode.getNodeType() == Node.ELEMENT_NODE && prevNode.getNodeName().equals(node.getNodeName())) {
-                i++;
-            }
-            prevNode = prevNode.getPreviousSibling();
-        }
-        return i;
     }
 }
