07213546df3c4f6da2838efb94b4a6a9ec693aa4
Carmen <carmen.iorga@1and1.ro>
2020-04-10 14:31:00 +0300

    refactor

--- src/main/java/com/cucumber/utils/engineering/utils/JsonUtils.java
+++ src/main/java/com/cucumber/utils/engineering/utils/JsonUtils.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.ValueNode;
+import org.apache.commons.lang3.tuple.Triple;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
@@ -36,61 +37,67 @@ public class JsonUtils {
 
     public void jsonParser() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode js = mapper.readTree("{\"status\": \"20*0\",\"Carm*en\": {\"autoRenew\": false,\"an|therField\": \"andot^erkey\"}}");
-        Map<String, String> finalMap = new HashMap<>();
-        addKeys("", js, finalMap, new ArrayList<>());
-        finalMap.entrySet()
-                .forEach(System.out::println);
-        checkRegexExists(finalMap);
+        JsonNode js = mapper.readTree("{\"status\":\"20*0\",\"fiel*d\":{\"autoRenew\":false,\"an|therField\":\"andot^erkey\",\"jsonArrayKey\":[\"^dbsjdb\",\"ccc.das\",\"c.aaa\"]}}");
+        Map<String, Map<String, String>> finalMap = new HashMap<>();
+        List<Triple<String, String, String>> tripleList = new ArrayList<>();
+        addKeys("", js, tripleList, new ArrayList<>());
+        tripleList.forEach(System.out::println);
+        checkRegexExists(tripleList);
 
     }
 
-    private void addKeys(String currentPath, JsonNode jsonNode, Map<String, String> mapToAdd, List<Integer> suffix) {
+    private void addKeys(String currentPath, JsonNode jsonNode, List<Triple<String, String, String>> tripleList, List<Integer> suffix) {
         if (jsonNode.isObject()) {
             ObjectNode objectNode = (ObjectNode) jsonNode;
             Iterator<Map.Entry<String, JsonNode>> iter = objectNode.fields();
-            String pathPrefix = currentPath.isEmpty() ? "" : currentPath + "-";
+            String pathPrefix = currentPath.isEmpty() ? "" : currentPath + "->";
             while (iter.hasNext()) {
                 Map.Entry<String, JsonNode> entry = iter.next();
                 if (entry.getValue().isObject()) {
-                    mapToAdd.put(entry.getKey(), "");
+                    tripleList.add(Triple.of(entry.getKey(), "", currentPath));
                 }
-                addKeys(pathPrefix + entry.getKey(), entry.getValue(), mapToAdd, suffix);
+                if (entry.getValue().isArray()) {
+                    tripleList.add(Triple.of(entry.getKey(), "", currentPath));
+                }
+                addKeys(pathPrefix + entry.getKey(), entry.getValue(), tripleList, suffix);
             }
 
         } else if (jsonNode.isArray()) {
+            System.out.println(jsonNode + "System.out.println -> jsonnode");
+            System.out.println(currentPath + "currentPath");
             ArrayNode arrayNode = (ArrayNode) jsonNode;
-
             for (int i = 0; i < arrayNode.size(); i++) {
                 suffix.add(i + 1);
-                addKeys(currentPath, arrayNode.get(i), mapToAdd, suffix);
-
-                if (i + 1 < arrayNode.size()) {
-                    suffix.remove(arrayNode.size() - 1);
-                }
+                addKeys(currentPath, arrayNode.get(i), tripleList, suffix);
             }
 
         } else if (jsonNode.isValueNode()) {
-            if (currentPath.contains("-")) {
-                for (int i = 0; i < suffix.size(); i++) {
-                    currentPath += "-" + suffix.get(i);
+            System.out.println("currentPath " + currentPath);
+            if (currentPath.contains("->")) {
+                StringBuilder currentPathBuilder = new StringBuilder(currentPath);
+                for (Integer integer : suffix) {
+                    currentPathBuilder.append("->").append(integer);
                 }
+                currentPath = currentPathBuilder.toString();
             }
             ValueNode valueNode = (ValueNode) jsonNode;
-            mapToAdd.put(currentPath, valueNode.asText());
+            tripleList.add(Triple.of(currentPath.split("->")[currentPath.split("->").length - 1],
+                    valueNode.asText(), currentPath));
         }
     }
 
-    public void checkRegexExists(Map<String, String> finalMap) {
+    public void checkRegexExists(List<Triple<String, String, String>> list) {
 
-        finalMap.forEach((key, value) -> {
-            List<String> regexFromKeys = RegexUtils.getRegexCharsFromString(key);
-            List<String> regexFromValue = RegexUtils.getRegexCharsFromString(value);
+        list.forEach(triple -> {
+            List<String> regexFromKeys = RegexUtils.getRegexCharsFromString(triple.getLeft());
+            List<String> regexFromValue = RegexUtils.getRegexCharsFromString(triple.getMiddle());
 
             if (!regexFromKeys.isEmpty())
-                log.warn("Regex " + regexFromKeys + " was found for key " + key + "at key-path: " + key);
+                log.warn("Regex " + regexFromKeys + " was found for key " + triple.getLeft()
+                        + " at key-path: " +
+                        (triple.getRight().equals("") ? triple.getLeft() : triple.getRight()));
             if (!regexFromValue.isEmpty())
-                log.warn("Regex " + regexFromValue + " was found for value " + value + " at key-path: " + key);
+                log.warn("Regex " + regexFromValue + " was found for value " + triple.getMiddle() + " at key-path: " + triple.getRight());
         });
     }
 
