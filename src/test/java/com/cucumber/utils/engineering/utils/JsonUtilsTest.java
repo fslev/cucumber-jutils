d70dcab20858ec77683076d6eb0291fe55df81ac
Florin Slevoaca <florin.slevoaca@ionos.com>
2020-04-22 11:09:59 +0300

    add test for root json array

--- src/test/java/com/cucumber/utils/engineering/utils/JsonUtilsTest.java
+++ src/test/java/com/cucumber/utils/engineering/utils/JsonUtilsTest.java
@@ -32,12 +32,22 @@ public class JsonUtilsTest {
         assertEquals(Arrays.asList("."), results.get("foo.bar/{key}"));
         assertTrue(Arrays.asList("?", "+").containsAll(results.get("foo.bar/a^b/{value}")));
         assertTrue(Arrays.asList("^").containsAll(results.get("foo.bar/a^b/{key}")));
-        assertTrue(Arrays.asList(".").containsAll(results.get("foo.bar/array[1]/a/{value}")));
-        assertTrue(Arrays.asList(".").containsAll(results.get("foo.bar/array[1]/.a/{key}")));
-        assertTrue(Arrays.asList("+").containsAll(results.get("foo.bar/array[1]/b/array[0]/{value}")));
-        assertTrue(Arrays.asList("+").containsAll(results.get("foo.bar/array[1]/b/array[1]/ips+um/{key}")));
-        assertTrue(Arrays.asList("[").containsAll(results.get("foo.bar/array[1]/b/array[1]/ips+um/{value}")));
-        assertTrue(Arrays.asList(".", "$").containsAll(results.get("foo.bar/array[1]/b/array[5]/{value}")));
+        assertTrue(Arrays.asList(".").containsAll(results.get("foo.bar/array[2]/a/{value}")));
+        assertTrue(Arrays.asList(".").containsAll(results.get("foo.bar/array[2]/.a/{key}")));
+        assertTrue(Arrays.asList("+").containsAll(results.get("foo.bar/array[2]/b/array[1]/{value}")));
+        assertTrue(Arrays.asList("+").containsAll(results.get("foo.bar/array[2]/b/array[2]/ips+um/{key}")));
+        assertTrue(Arrays.asList("[").containsAll(results.get("foo.bar/array[2]/b/array[2]/ips+um/{value}")));
+        assertTrue(Arrays.asList(".", "$").containsAll(results.get("foo.bar/array[2]/b/array[6]/{value}")));
         assertEquals(9, results.size());
+    }
+
+    @Test
+    public void testSimpleJsonSpecialRegexCharactersFromArray() throws ParserConfigurationException, SAXException, IOException {
+        String json = "[\"te?st\", {\"^a..\":\"?\"}, false]";
+        Map<String, List<String>> results = JsonUtils.walkJsonAndProcessNodes(json, extractSpecialRegexCharsFct);
+        assertEquals(Arrays.asList("?"), results.get("[1]/{value}"));
+        assertTrue(Arrays.asList(".", "^").containsAll(results.get("[2]/^a../{key}")));
+        assertTrue(Arrays.asList("?").containsAll(results.get("[2]/^a../{value}")));
+        assertEquals(3, results.size());
     }
 }
