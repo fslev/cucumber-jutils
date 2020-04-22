a08b25d8fb90181ba906da9aebb93add9043e772
Florin Slevoaca <florin.slevoaca@ionos.com>
2020-04-22 11:09:24 +0300

    compact code

--- src/main/java/com/cucumber/utils/engineering/utils/Json.java
+++ src/main/java/com/cucumber/utils/engineering/utils/Json.java
@@ -1,41 +1,29 @@
 package com.cucumber.utils.engineering.utils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeType;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.util.AbstractMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.function.Function;
 
 public class Json {
 
 
-    static <R> void walkAndProcessJson(JsonNode jsonNode, Function<String, R> processFunction, String parentPath, Map<String, R> results) throws JsonProcessingException {
-        Map.Entry<String, R> result = null;
+    static <R> void walkAndProcessJson(JsonNode jsonNode, Function<String, R> processFunction, String parentPath, Map<String, R> results) {
+        String pathPrefix = parentPath.isEmpty() ? "" : parentPath + "/";
         if (jsonNode.isObject()) {
-            addNewResultToMap(results, result);
-            ObjectNode objectNode = (ObjectNode) jsonNode;
-            Iterator<Map.Entry<String, JsonNode>> iter = objectNode.fields();
-            String pathPrefix = parentPath.isEmpty() ? "" : parentPath + "/";
-            while (iter.hasNext()) {
-                Map.Entry<String, JsonNode> entry = iter.next();
-                result = processKey(pathPrefix + entry.getKey(), entry.getKey(), processFunction);
-                addNewResultToMap(results, result);
-                walkAndProcessJson(entry.getValue(), processFunction, pathPrefix + entry.getKey(), results);
-            }
-
+            jsonNode.fields().forEachRemaining(field -> {
+                String currentPath = pathPrefix + field.getKey();
+                addNewResultToMap(results, processKey(currentPath, field.getKey(), processFunction));
+                walkAndProcessJson(field.getValue(), processFunction, currentPath, results);
+            });
         } else if (jsonNode.isArray()) {
-            ArrayNode arrayNode = (ArrayNode) jsonNode;
-            for (int i = 0; i < arrayNode.size(); i++) {
-                walkAndProcessJson(jsonNode.get(i), processFunction, parentPath + "[" + i + "]", results);
+            for (int i = 0; i < jsonNode.size(); i++) {
+                walkAndProcessJson(jsonNode.get(i), processFunction, parentPath + "[" + (i + 1) + "]", results);
             }
         } else if (jsonNode.getNodeType().equals(JsonNodeType.STRING)) {
-            result = processValue(parentPath, jsonNode, processFunction);
-            addNewResultToMap(results, result);
+            addNewResultToMap(results, processValue(parentPath, jsonNode, processFunction));
         }
     }
 
