829d5b4222b4d957792cb6aa550c558e334510e3
Carmen <carmen.iorga@1and1.ro>
2020-04-23 12:03:20 +0300

    add common steps for comments

---
+++ src/main/java/com/cucumber/utils/context/stepdefs/CommonSteps.java
@@ -0,0 +1,36 @@
+package com.cucumber.utils.context.stepdefs;
+
+import io.cucumber.guice.ScenarioScoped;
+import io.cucumber.java.en.Given;
+
+@ScenarioScoped
+public class CommonSteps {
+
+    /**
+     * Cucumbe native JSON reporting plugin does not support comments
+     * Therefore, the step from bellow simulates comments
+     *
+     * @param s
+     */
+    @Given("^#[^><](.+)$")
+    public void comment(String s) {
+    }
+
+    /**
+     * Add collapsible support to Cucumber maven reporting plugin
+     *
+     * @param s
+     */
+    @Given("^#>>(.+)$")
+    public void startCollapsible(String s) {
+    }
+
+    @Given("^#<<$")
+    public void endCollapsible() {
+    }
+
+    @Given("Wait {}s")
+    public void wait(int until) throws InterruptedException {
+        Thread.sleep(until * 1000);
+    }
+}
