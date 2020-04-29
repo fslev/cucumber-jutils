9e540a75afb97ef0634c297d77db6e7c2f2bd035
Carmen <carmen.iorga@1and1.ro>
2020-04-29 15:24:42 +0300

    - Add date format support for scenario properties using custom date - moved dateTime pattern inside every dateTime step

--- src/main/java/com/cucumber/utils/context/stepdefs/DateTimeSteps.java
+++ src/main/java/com/cucumber/utils/context/stepdefs/DateTimeSteps.java
@@ -4,7 +4,6 @@ import com.cucumber.utils.context.props.ScenarioProps;
 import com.cucumber.utils.context.utils.ScenarioUtils;
 import com.google.inject.Inject;
 import io.cucumber.guice.ScenarioScoped;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
 import java.time.Duration;
@@ -27,77 +26,80 @@ public class DateTimeSteps {
         PLUS, MINUS
     }
 
-    @Given("DateTime pattern=\"{}\"")
-    public void format(String pattern) {
-        this.formatter = DateTimeFormatter.ofPattern(pattern);
-    }
-
-    @Then("DateTime check period from \"{}\" to \"{}\" is {}year(s)")
-    public void compareYears(String date1, String date2, Object y) {
+    @Then("DateTime with format {} check period from \"{}\" to \"{}\" is {}year(s)")
+    public void compareYears(String format, String date1, String date2, Object y) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format);
         logger.log("Check period from '{}' to '{}' is {}years", date1, date2, y);
-        LocalDateTime localDateTime1 = LocalDateTime.parse(date1, formatter);
-        LocalDateTime localDateTime2 = LocalDateTime.parse(date2, formatter);
+        LocalDateTime localDateTime1 = LocalDateTime.parse(date1, dateTimeFormatter);
+        LocalDateTime localDateTime2 = LocalDateTime.parse(date2, dateTimeFormatter);
         assertEquals("Years differ", Long.parseLong(y.toString()), ChronoUnit.YEARS.between(localDateTime1, localDateTime2));
     }
 
-    @Then("DateTime check period from \"{}\" to \"{}\" is {}day(s)")
-    public void compareDays(String date1, String date2, Object d) {
+    @Then("DateTime with format {} check period from \"{}\" to \"{}\" is {}day(s)")
+    public void compareDays(String format, String date1, String date2, Object d) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format);
         logger.log("Check period from '{}' to '{}' is {}days", date1, date2, d);
-        LocalDateTime localDateTime1 = LocalDateTime.parse(date1, formatter);
-        LocalDateTime localDateTime2 = LocalDateTime.parse(date2, formatter);
+        LocalDateTime localDateTime1 = LocalDateTime.parse(date1, dateTimeFormatter);
+        LocalDateTime localDateTime2 = LocalDateTime.parse(date2, dateTimeFormatter);
         assertEquals("Days differ", Long.parseLong(d.toString()), ChronoUnit.DAYS.between(localDateTime1, localDateTime2));
     }
 
-    @Then("DateTime check period from \"{}\" to \"{}\" is {}hour(s)")
-    public void compareHours(String date1, String date2, Object h) {
+    @Then("DateTime with format {} check period from \"{}\" to \"{}\" is {}hour(s)")
+    public void compareHours(String format, String date1, String date2, Object h) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format);
         logger.log("Check period from '{}' to '{}' is {}hours", date1, date2, h);
-        LocalDateTime localDateTime1 = LocalDateTime.parse(date1, formatter);
-        LocalDateTime localDateTime2 = LocalDateTime.parse(date2, formatter);
+        LocalDateTime localDateTime1 = LocalDateTime.parse(date1, dateTimeFormatter);
+        LocalDateTime localDateTime2 = LocalDateTime.parse(date2, dateTimeFormatter);
         assertEquals("Hours differ", Long.parseLong(h.toString()), Duration.between(localDateTime1, localDateTime2).toHours());
     }
 
-    @Then("DateTime check period from \"{}\" to \"{}\" is {}minute(s)")
-    public void compareMinutes(String date1, String date2, Object m) {
+    @Then("DateTime with format {} check period from \"{}\" to \"{}\" is {}minute(s)")
+    public void compareMinutes(String format, String date1, String date2, Object m) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format);
         logger.log("Check period from '{}' to '{}' is {}mins", date1, date2, m);
-        LocalDateTime localDateTime1 = LocalDateTime.parse(date1, formatter);
-        LocalDateTime localDateTime2 = LocalDateTime.parse(date2, formatter);
+        LocalDateTime localDateTime1 = LocalDateTime.parse(date1, dateTimeFormatter);
+        LocalDateTime localDateTime2 = LocalDateTime.parse(date2, dateTimeFormatter);
         assertEquals("Minutes differ", Long.parseLong(m.toString()), Duration.between(localDateTime1, localDateTime2).toMinutes());
     }
 
-    @Then("DateTime check period from \"{}\" to \"{}\" is {}second(s)")
-    public void compareSeconds(String date1, String date2, Object s) {
+    @Then("DateTime with format {} check period from \"{}\" to \"{}\" is {}second(s)")
+    public void compareSeconds(String format, String date1, String date2, Object s) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format);
         logger.log("Check period from '{}' to '{}' is {}seconds", date1, date2, s);
-        LocalDateTime localDateTime1 = LocalDateTime.parse(date1, formatter);
-        LocalDateTime localDateTime2 = LocalDateTime.parse(date2, formatter);
+        LocalDateTime localDateTime1 = LocalDateTime.parse(date1, dateTimeFormatter);
+        LocalDateTime localDateTime2 = LocalDateTime.parse(date2, dateTimeFormatter);
         assertEquals("Seconds differ", Long.parseLong(s.toString()), Duration.between(localDateTime1, localDateTime2).getSeconds());
     }
 
-    @Then("Date check period from \"{}\" to \"{}\" is {}year(s)")
-    public void compareYearsFromDates(String date1, String date2, Object y) {
+    @Then("Date with format {} check period from \"{}\" to \"{}\" is {}year(s)")
+    public void compareYearsFromDates(String format, String date1, String date2, Object y) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format);
         logger.log("Check date period from '{}' to '{}' is {}years", date1, date2, y);
-        LocalDate localDate1 = LocalDateTime.parse(date1, formatter).toLocalDate();
-        LocalDate localDate2 = LocalDateTime.parse(date2, formatter).toLocalDate();
+        LocalDate localDate1 = LocalDateTime.parse(date1, dateTimeFormatter).toLocalDate();
+        LocalDate localDate2 = LocalDateTime.parse(date2, dateTimeFormatter).toLocalDate();
         assertEquals("Years differ", Long.parseLong(y.toString()), ChronoUnit.YEARS.between(localDate1, localDate2));
     }
 
-    @Then("Date check period from \"{}\" to \"{}\" is {}months(s)")
-    public void compareMonthsFromDates(String date1, String date2, Object m) {
-        logger.log("Check date period from '{}' to '{}' is {}years", date1, date2, m);
-        LocalDate localDate1 = LocalDateTime.parse(date1, formatter).toLocalDate();
-        LocalDate localDate2 = LocalDateTime.parse(date2, formatter).toLocalDate();
+    @Then("Date with format {} check period from \"{}\" to \"{}\" is {}month(s)")
+    public void compareMonthsFromDates(String format, String date1, String date2, Object m) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format);
+        logger.log("Check date period from '{}' to '{}' is {}months", date1, date2, m);
+        LocalDate localDate1 = LocalDateTime.parse(date1, dateTimeFormatter).toLocalDate();
+        LocalDate localDate2 = LocalDateTime.parse(date2, dateTimeFormatter).toLocalDate();
         assertEquals("Years differ", Long.parseLong(m.toString()), ChronoUnit.MONTHS.between(localDate1, localDate2));
     }
 
-    @Then("Date check period from \"{}\" to \"{}\" is {}day(s)")
-    public void compareDaysFromDates(String date1, String date2, Object d) {
+    @Then("Date with format {} check period from \"{}\" to \"{}\" is {}day(s)")
+    public void compareDaysFromDates(String format, String date1, String date2, Object d) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format);
         logger.log("Check date period from '{}' to '{}' is {}days", date1, date2, d);
-        LocalDate localDate1 = LocalDateTime.parse(date1, formatter).toLocalDate();
-        LocalDate localDate2 = LocalDateTime.parse(date2, formatter).toLocalDate();
+        LocalDate localDate1 = LocalDateTime.parse(date1, dateTimeFormatter).toLocalDate();
+        LocalDate localDate2 = LocalDateTime.parse(date2, dateTimeFormatter).toLocalDate();
         assertEquals("Days differ", Long.parseLong(d.toString()), ChronoUnit.DAYS.between(localDate1, localDate2));
     }
 
     @Then("date param {}=\"now {} {} {}\" with format pattern={}")
-    public void formatDate(String param, Operation operation, int value, ChronoUnit chronoUnit, String formatPattern) {
+    public void formatDateNow(String param, Operation operation, int value, ChronoUnit chronoUnit, String formatPattern) {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(formatPattern);
         switch (operation) {
             case MINUS:
@@ -107,5 +109,16 @@ public class DateTimeSteps {
         }
     }
 
+    @Then("date param {}=\"from {} {} {} {}\" with format pattern={}")
+    public void setDateFormattedParam(String param, String date, Operation operation, int value, ChronoUnit chronoUnit, String formatPattern) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(formatPattern);
+        switch (operation) {
+            case MINUS:
+                scenarioProps.put(param, LocalDateTime.parse(date, dateTimeFormatter).minus(value, chronoUnit).format(dateTimeFormatter));
+            case PLUS:
+                scenarioProps.put(param, LocalDateTime.parse(date, dateTimeFormatter).plus(value, chronoUnit).format(dateTimeFormatter));
+        }
+
+    }
 
 }
--- src/test/resources/features/Date.feature
+++ src/test/resources/features/Date.feature
@@ -1,31 +1,41 @@
 Feature: Date feature
 
   Scenario: Compare two time dates
-    Given DateTime pattern="yyyy-MM-dd HH:mm:ss"
-    Then DateTime check period from "2018-02-03 01:00:00" to "2019-02-03 01:00:00" is 1year
-    And DateTime check period from "2018-02-03 01:00:00" to "2019-02-02 12:01:10" is 364days
-    And DateTime check period from "2019-02-03 01:02:12" to "2019-02-03 23:59:10" is 22hours
+    Then DateTime with format yyyy-MM-dd HH:mm:ss check period from "2018-02-03 01:00:00" to "2019-02-03 01:00:00" is 1year
+    And DateTime with format yyyy-MM-dd HH:mm:ss check period from "2018-02-03 01:00:00" to "2019-02-02 12:01:10" is 364days
+    And DateTime with format yyyy-MM-dd HH:mm:ss check period from "2019-02-03 01:02:12" to "2019-02-03 23:59:10" is 22hours
     Given param expected="116"
-    And DateTime check period from "2019-02-03 22:02:12" to "2019-02-03 23:59:10" is #[expected]minutes
+    And DateTime with format yyyy-MM-dd HH:mm:ss check period from "2019-02-03 22:02:12" to "2019-02-03 23:59:10" is #[expected]minutes
     Given param expected="58"
-    And DateTime check period from "2019-02-03 23:58:12" to "2019-02-03 23:59:10" is #[expected]seconds
+    And DateTime with format yyyy-MM-dd HH:mm:ss check period from "2019-02-03 23:58:12" to "2019-02-03 23:59:10" is #[expected]seconds
 
   Scenario: Compare two dates
-    Given DateTime pattern="yyyy-MM-dd HH:mm:ss"
-    Then Date check period from "2018-02-03 01:00:00" to "2019-02-03 00:00:00" is 1year
-    Then Date check period from "2018-02-03 01:00:00" to "2019-02-03 00:00:00" is 365days
-    Then Date check period from "2020-02-03 01:00:00" to "2021-02-03 00:00:00" is 1year
-    Then Date check period from "2020-02-03 01:00:00" to "2021-02-03 00:00:00" is 366days
+    Then Date with format yyyy-MM-dd HH:mm:ss check period from "2018-02-03 01:00:00" to "2019-02-03 00:00:00" is 1year
+    Then Date with format yyyy-MM-dd HH:mm:ss check period from "2018-02-03 01:00:00" to "2019-02-03 00:00:00" is 365days
+    Then Date with format yyyy-MM-dd HH:mm:ss check period from "2020-02-03 01:00:00" to "2021-02-03 00:00:00" is 1year
+    Then Date with format yyyy-MM-dd HH:mm:ss check period from "2020-02-03 01:00:00" to "2021-02-03 00:00:00" is 366days
 
-    Scenario: Format dates
-      Given DateTime pattern="yyyy-MM-dd HH:mm:ss"
-      Given date param currentTime="now PLUS 0 YEARS" with format pattern=yyyy-MM-dd HH:mm:ss
-      And date param years="now PLUS 15 YEARS" with format pattern=yyyy-MM-dd HH:mm:ss
-      And date param days="now PLUS 15 DAYS" with format pattern=yyyy-MM-dd HH:mm:ss
-      And date param months="now PLUS 15 MONTHS" with format pattern=yyyy-MM-dd HH:mm:ss
-      And date param minutes="now PLUS 15 MINUTES" with format pattern=yyyy-MM-dd HH:mm:ss
+  Scenario: Format dates from current date
+    Given date param currentTime="now PLUS 0 YEARS" with format pattern=yyyy-MM-dd HH:mm:ss
+    And date param years="now PLUS 15 YEARS" with format pattern=yyyy-MM-dd HH:mm:ss
+    And date param days="now PLUS 15 DAYS" with format pattern=yyyy-MM-dd HH:mm:ss
+    And date param months="now PLUS 15 MONTHS" with format pattern=yyyy-MM-dd HH:mm:ss
+    And date param minutes="now PLUS 15 MINUTES" with format pattern=yyyy-MM-dd HH:mm:ss
 
-      Then Date check period from "#[currentTime]" to "#[years]" is 15year
-      Then Date check period from "#[currentTime]" to "#[days]" is 15days
-      Then Date check period from "#[currentTime]" to "#[months]" is 15months
-      Then DateTime check period from "#[currentTime]" to "#[minutes]" is 15minutes
+    Then Date with format yyyy-MM-dd HH:mm:ss check period from "#[currentTime]" to "#[years]" is 15year
+    Then Date with format yyyy-MM-dd HH:mm:ss check period from "#[currentTime]" to "#[days]" is 15days
+    Then Date with format yyyy-MM-dd HH:mm:ss check period from "#[currentTime]" to "#[months]" is 15months
+    Then DateTime with format yyyy-MM-dd HH:mm:ss check period from "#[currentTime]" to "#[minutes]" is 15minutes
+
+
+  Scenario: Format dates from custom date
+    Given date param currentTime="from 2020-04-28 18:05:58 PLUS 0 YEARS" with format pattern=yyyy-MM-dd HH:mm:ss
+    And date param years="from 2020-04-28 18:05:58 PLUS 1 YEARS" with format pattern=yyyy-MM-dd HH:mm:ss
+    And date param months="from 2020-04-28 18:05:58 PLUS 1 MONTHS" with format pattern=yyyy-MM-dd HH:mm:ss
+    And date param days="from 2020-04-28 18:05:58 PLUS 1 DAYS" with format pattern=yyyy-MM-dd HH:mm:ss
+    And date param minutes="from 2020-04-28 18:05:58 PLUS 1 MINUTES" with format pattern=yyyy-MM-dd HH:mm:ss
+
+    Then Date with format yyyy-MM-dd HH:mm:ss check period from "#[currentTime]" to "#[years]" is 1year
+    Then Date with format yyyy-MM-dd HH:mm:ss check period from "#[currentTime]" to "#[days]" is 1days
+    Then Date with format yyyy-MM-dd HH:mm:ss check period from "#[currentTime]" to "#[months]" is 1months
+    Then DateTime with format yyyy-MM-dd HH:mm:ss check period from "#[currentTime]" to "#[minutes]" is 1minutes
