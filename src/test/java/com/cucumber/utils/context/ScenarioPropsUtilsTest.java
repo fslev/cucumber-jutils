2e4bbd1688a8deea423cb1aba0807e1ce112e06a
Florin Slevoaca <florin.slevoaca@ionos.com>
2021-07-29 15:07:58 +0300

    more tests

---
+++ src/test/java/com/cucumber/utils/context/ScenarioPropsUtilsTest.java
@@ -0,0 +1,22 @@
+package com.cucumber.utils.context;
+
+import com.cucumber.utils.context.props.ScenarioProps;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThrows;
+
+public class ScenarioPropsUtilsTest {
+
+    private ScenarioProps scenarioProps;
+
+    @Before
+    public void init() {
+        this.scenarioProps = new ScenarioProps();
+    }
+
+    @Test
+    public void testParserFromInvalidFile() {
+        assertThrows(RuntimeException.class, () -> ScenarioPropsUtils.parse("idontexist.json", scenarioProps));
+    }
+}
