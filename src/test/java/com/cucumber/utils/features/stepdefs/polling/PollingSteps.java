8334965b08ba2811f7cd3833d21defb905197979
fslevoaca <florin.slevoaca@ionos.com>
2026-05-07 11:25:00 +0300

    Update jtest.utils to version 7.0 and refactor polling logic to match new API

--- src/test/java/com/cucumber/utils/features/stepdefs/polling/PollingSteps.java
+++ src/test/java/com/cucumber/utils/features/stepdefs/polling/PollingSteps.java
@@ -6,6 +6,7 @@ import io.cucumber.java.en.Then;
 import io.jtest.utils.matcher.ObjectMatcher;
 
 import java.time.Duration;
+import java.util.function.Supplier;
 
 @ScenarioScoped
 public class PollingSteps {
@@ -21,12 +22,26 @@ public class PollingSteps {
 
     @Then("poll until random generated number {int} is found")
     public void pollFor(int expected) {
-        ObjectMatcher.match(null, expected, this::generateRandom, Duration.ofSeconds(30), null, null);
+        pollUntilMatch(expected, this::generateRandom, Duration.ofSeconds(30));
     }
 
     @Then("poll {} until 0=0")
     public void pollForZeroSeconds(Duration pollingDuration) {
-        ObjectMatcher.match(null, 0, () -> 0, pollingDuration, null, null);
+        pollUntilMatch(0, () -> 0, pollingDuration);
+    }
+
+    private static void pollUntilMatch(Object expected, Supplier<Object> actualSupplier, Duration pollingDuration) {
+        long deadline = System.nanoTime() + pollingDuration.toNanos();
+        RuntimeException lastFailure;
+        do {
+            try {
+                ObjectMatcher.match(null, expected, actualSupplier.get());
+                return;
+            } catch (RuntimeException e) {
+                lastFailure = e;
+            }
+        } while (System.nanoTime() < deadline);
+        throw lastFailure;
     }
 
     public int generateRandom() {
